529a6074635e6bd5b4e99fdad6818337863f6da4
prashanthanantha <prashanthanantham@gmail.com>
2021-05-01 00:30:47 +0530

    add Launches compones

---
+++ src/Components/Launches/Launches.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Launch } from '../Launch/Launch.jsx';
+import './styles.css';
+export class LaunchesList extends React.Component{
+    render(){
+        return(
+            <div className ="launches-list">
+                <Launch 
+      banner="https://farm8.staticflickr.com/7624/17170624642_e5949d160e_o.jpg"
+      title ="FalconSat"
+      date = "2006-03-25"
+      description="Ratsat was carried to orbit on the first successful orbital launch of any privately funded and developed, liquid-propelled carrier rocket, the SpaceX Falcon 1
+      Ratsat was carried to orbit on the first successful orbital launch of any privately funded and developed, liquid-propelled carrier rocket, the SpaceX Falcon 1
+      Ratsat was carried to orbit on the first successful orbital launch of any privately funded and developed, liquid-propelled carrier rocket, the SpaceX Falcon 1
+      Ratsat was carried to orbit on the first successful orbital launch of any privately funded and developed, liquid-propelled carrier rocket, the SpaceX Falcon 1"
+      />
+      <Launch 
+      banner="https://farm8.staticflickr.com/7624/17170624642_e5949d160e_o.jpg"
+      title ="FalconSat"
+      date = "2006-03-25"
+      description="Ratsat was carried to orbit on the first successful orbital launch of any privately funded and developed, liquid-propelled carrier rocket, the SpaceX Falcon 1
+      Ratsat was carried to orbit on the first successful orbital launch of any privately funded and developed, liquid-propelled carrier rocket, the SpaceX Falcon 1
+      Ratsat was carried to orbit on the first successful orbital launch of any privately funded and developed, liquid-propelled carrier rocket, the SpaceX Falcon 1
+      Ratsat was carried to orbit on the first successful orbital launch of any privately funded and developed, liquid-propelled carrier rocket, the SpaceX Falcon 1"
+      />
+      <Launch 
+      banner="https://farm8.staticflickr.com/7624/17170624642_e5949d160e_o.jpg"
+      title ="FalconSat"
+      date = "2006-03-25"
+      description="Ratsat was carried to orbit on the first successful orbital launch of any privately funded and developed, liquid-propelled carrier rocket, the SpaceX Falcon 1
+      Ratsat was carried to orbit on the first successful orbital launch of any privately funded and developed, liquid-propelled carrier rocket, the SpaceX Falcon 1
+      Ratsat was carried to orbit on the first successful orbital launch of any privately funded and developed, liquid-propelled carrier rocket, the SpaceX Falcon 1
+      Ratsat was carried to orbit on the first successful orbital launch of any privately funded and developed, liquid-propelled carrier rocket, the SpaceX Falcon 1"
+      />
+            </div>
+        )
+    }
+}
